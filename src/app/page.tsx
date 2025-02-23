30aa2f03467ee40dff66d2c69ccfd4db9eee7200
Psychohistorian11 <cristianfrancoraigosa@gmail.com>
2025-02-22 20:18:45 -0500

    deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
-            href="login"
+            href="/login"
             className="rounded-lg border border-solid border-transparent transition-colors flex items-center justify-center 
             bg-foreground text-background gap-2 hover:bg-gray-600 dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-40"
           >
@@ -36,7 +36,7 @@ export default function Home() {
           </Link>
 
           <Link
-            href="register"
+            href="/register"
             className="rounded-lg border border-solid bg-gray-300 transition-colors flex items-center 
             justify-center hover:bg-gray-600 hover:text-white hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-40"
           >
