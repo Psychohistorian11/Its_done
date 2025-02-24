19eb0e9a60979fa39492b38411ad74dcbde08ffc
Psychohistorian11 <cristianfrancoraigosa@gmail.com>
2025-02-23 21:14:44 -0500

    deploy#2

--- src/components/services/route.tsx
+++ src/components/services/route.tsx
@@ -3,11 +3,19 @@
 import { signIn, signOut } from "../../auth";
 
 export async function handleGoogleSignIn() {
-  await signIn("google", { redirect: true, redirectTo: "/task" });
+  await signIn("google", {
+    redirect: true,
+    redirectTo: "/task",
+    callbackUrl: "https://its-done.onrender.com",
+  });
 }
 
 export async function handleGithubSignIn() {
-  await signIn("github", { redirect: true, redirectTo: "/task" });
+  await signIn("github", {
+    redirect: true,
+    redirectTo: "/task",
+    callbackUrl: "https://its-done.onrender.com",
+  });
 }
 
 export async function handleSignOut() {
